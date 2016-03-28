ca78650900c500a2e37a5fd07f3f13b3ba873d63
Elkanah Werther <wertherei@gmail.com>
2016-03-28 12:40:43 -0400

    initial commit

---
+++ HelloGit/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloGit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public void Index()
+        {
+            Response.Write("<h1>Hello GitHub</>");
+        }
+
+    }
+}
